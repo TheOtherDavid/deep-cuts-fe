5ac4ef726baced808d06831349084e2ed7061081
David <davidhand1988@gmail.com>
2022-12-11 13:57:35 -0500

    The UI works, and sends the token to the backend to call the functions.

--- src/App.js
+++ src/App.js
@@ -3,69 +3,72 @@ import React from 'react';
 import { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-//import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Login from './Login'
 
 const code = new URLSearchParams(window.location.search).get('code')
 
 function App() {
-  const [playlist, setPlaylist] = useState("");
   const [token, setToken] = useState("");
-  //return code ? <GenerateForm code={code} /> : <Login />
-  if (code == null && token == '') {
-    return <Login />
-  } else {
-    return <>
-     <GetToken />
-     <GetPlaylistForm code={code} />
-     <GeneratePlaylist code={code} />
-    </>
-  }
-}
+
+  return (
+    <div>
+      {!token && <Login onLogin={setToken} />}
+      {token && <GetPlaylistForm token={token} />}
+      {token && <GeneratePlaylist token={token} />}
+    </div>
+  );
+};
 
 export default App;
 
-function GetPlaylistForm() {
-    const [playlistId, setPlaylistId] = useState("");
-    const [playlists, setPlaylists] = useState("");
-    
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        
-        const resp = await axios.get(`http://localhost:8080/${playlistId}?code=${code}`)
-        const playlists = resp.data
+function GetPlaylistForm({ token }) {
+  const [playlistId, setPlaylistId] = useState("");
+  const [playlists, setPlaylists] = useState("");
 
-        setPlaylists(playlists);
-    };
-    return <>
-        <form onSubmit={handleSubmit}>
-            <input type="text" 
-            name="playlistId"
-            placeholder="Playlist ID" 
-            onChange={(e) => setPlaylistId(e.target.value)} />
-            <button type="submit">Get Playlist!</button>
-        </form>
-        {(playlistId === "" || playlistId === "undefined") &&
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const resp = await axios.get(`http://localhost:8080/${playlistId}`,
+    {
+      headers: {
+        'Authorization': "Bearer " + token
+      }
+    });
+    const playlists = resp.data;
+
+    setPlaylists(playlists);
+  };
+
+  return (
+    <div className="main">
+      <form onSubmit={handleSubmit}>
+          <input type="text" 
+          name="playlistId"
+          placeholder="Playlist ID" 
+          onChange={(e) => setPlaylistId(e.target.value)} />
+          <button type="submit">Get Playlist!</button>
+      </form>
+      {(playlistId === "" || playlistId === "undefined") &&
+      <div>
+        Enter a Playlist ID.
+      </div>
+      }
+      <div>
+      {(playlists === "" || playlists === undefined) &&
+      <div>No list</div>}
+      {playlists && playlists.map(playlist =>
         <div>
-          Enter a Playlist ID.
+              <div>Track: </div>
+              <img src={playlist.album.images[2].url}></img>
+              <div>Song: {playlist.name}</div>
+              <div>Artist: {playlist.artists[0].name}</div>
+              <a href={playlist.external_urls.spotify} target="_blank">Listen!</a>
+              <div>Link: {playlist.external_urls.spotify}</div>
         </div>
-        }
-        <div>
-        {(playlists === "" || playlists === undefined) &&
-        <div>No list</div>}
-        {playlists && playlists.map(playlist =>
-          <div>
-                <div>Track: </div>
-                <img src={playlist.album.images[2].url}></img>
-                <div>Song: {playlist.name}</div>
-                <div>Artist: {playlist.artists[0].name}</div>
-                <a href={playlist.external_urls.spotify} target="_blank">Listen!</a>
-                <div>Link: {playlist.external_urls.spotify}</div>
-          </div>
-      )}
-        </div>
-    </>;
+    )}
+      </div>
+    </div>
+  );
 }
 
 function ListPlaylistTracks() {
@@ -80,17 +83,21 @@ function ListPlaylistTracks() {
 //<div>This is List ${playlist[0].artists[0].name}</div> }
 }
 
-function GeneratePlaylist() {
+function GeneratePlaylist({ token }) {
   const [playlistId, setPlaylistId] = useState("");
   const [playlists, setPlaylists] = useState("");
   
   const handleSubmit = async (event) => {
-      event.preventDefault();
-      
-      const resp = await axios.post(`http://localhost:8080/${playlistId}?code=${code}`)
-      const playlists = resp.data
+    event.preventDefault();
+    const resp = await axios.post(`http://localhost:8080/${playlistId}`, null,
+    {
+      headers: {
+        'Authorization': "Bearer " + token
+      }
+    })
+    const playlists = resp.data
 
-      setPlaylists(playlists);
+    setPlaylists(playlists);
   };
   return <>
     <div className="main">
@@ -126,12 +133,12 @@ function GeneratePlaylist() {
   </>
 }
 
-function GetToken() {
-  const [token, setToken] = useState([]);
+function GetToken({ setToken }) {
+  //const [tokenState, setToken] = useState([]);
   useEffect(async() => {
     const resp = await axios.get(`http://localhost:8080/token?code=${code}`)
-    const token = resp.token
-
+    const token = resp.data.token
+    //Are we setting the state properly here?
     setToken(token);
   }, []);
 
--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,36 @@
-import React from "react"
-import { Container } from "react-bootstrap"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const AUTH_URL = 
-`https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}\u0026redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F\u0026response_type=code\u0026scope=playlist-modify-private+playlist-modify-public\u0026state=abc123`
+const Login = ({ onLogin }) => {
+  const [code, setCode] = useState(null);
+  const [token, setToken] = useState(null);
 
-export default function Login() {
-    return (
-    <Container 
-        className="d-flex justify-content-center align-items-center"
-        style={{ minHeight: "100vh" }}
-    >
-        <a className="btn btn-success btn-lg" href={AUTH_URL}>
-            Login With Spotify
-        </a>
-    </Container>
-    )
-}
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    setCode(searchParams.get('code'));
+  }, []);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const resp = await axios.get(`http://localhost:8080/token?code=${code}`);
+      onLogin(resp.data.token);
+    };
+
+    if (code) {
+      fetchToken();
+    }
+  }, [code, onLogin]);
+
+  const handleLogin = () => {
+    window.location = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}\u0026redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F\u0026response_type=code\u0026scope=playlist-modify-private+playlist-modify-public\u0026state=abc123`;
+  };
+
+  return (
+    <div>
+      {!token && (
+        <button onClick={handleLogin}>Login with Spotify</button>
+      )}
+    </div>
+  );
+};
+export default Login;
